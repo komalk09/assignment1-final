984536fc28c70b540472a1486bab1c45894f3c7a
komalk09 <103804778+komalk09@users.noreply.github.com>
2022-06-06 13:41:24 -0400

    updated sg

--- terraform_code/dev/instances/main.tf
+++ terraform_code/dev/instances/main.tf
@@ -85,7 +85,22 @@ resource "aws_security_group" "my_sg" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-
+ ingress {
+    description      = "Opening port 8080"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+   ingress {
+    description      = "Opening port 8081"
+    from_port        = 8081
+    to_port          = 8081
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
   egress {
     from_port        = 0
     to_port          = 0
